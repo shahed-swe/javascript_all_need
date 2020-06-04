259962be0d8309f200b8b6b46e5e7a545843205a
Shahed Talukder <shahedtalukder51@gmail.com>
2020-06-04 12:30:12 +0600

    first

---
+++ object.js
@@ -0,0 +1,15 @@
+var new_stu = {
+    "name": "Shahed",
+    "age": 21,
+    "title": "Talukder",
+    "address": "adalotpara"
+}
+
+console.log(new_stu);
+
+new_stu.age = 23;
+console.log(new_stu);
+
+new_stu.gender = "male";
+
+console.log(new_stu)
